97cd31ab494b619a0b8022698aa24d1777df1c21
Robert McLeod <robert@autogrow.com>
2016-10-04 12:38:42 +1300

    tidied up code with linter, removed clustering, added debug

--- index.js
+++ index.js
@@ -1,78 +1,70 @@
-'use strict';
+"use strict";
+
 /**
  * Dependencies
  **/
-var io = require('socket.io'),
-  ioAuth = require('socketio-auth'),
-  Pubsub = require('./pubsub')
+var io = require("socket.io"),
+  ioAuth = require("socketio-auth"),
+  Pubsub = require("./pubsub"),
+  debug = require("debug")("lc:pubsub");
+
 /**
  * @module LoopBack Component PubSub
  * @author Jonathan Casarrubias <http://twitter.com/johncasarrubias>
  * @description
+ *
  * This module integrates LoopBack with Socket IO in order to provide
  * PubSub functionallity.
- * 
- * TODO:
- * 
- * Clusterize with socket.io adapter
  */
 module.exports = (app, options) => {
+
   /**
    * Set Default Options
    */
   options = Object.assign({}, options, {
     auth: true,
-    debug: false,
-    cluster: {
-      enabled    : false,
-      adapter    : 'socket.io-adapter-mongo',
-      datasource : 'mongodb'
-    }
   });
+
   /**
    * Set Listener waiting for Http Server
    **/
-  app.on('started', start);
+  app.on("started", start);
+
   /**
    * Setup Real Time Communications
    **/
   function start(server) {
-    console.info('RTC server listening at %s', app.get('url').replace('http', 'ws'));
+    debug("RTC server listening at %s", app.get("url").replace("http", "ws"));
     // Lets create an instance of IO and reference it in app
     var socket = io(server);
-    // Enable Clustering Capabilities
-    if (options.cluster.enabled) {
-      try {
-        let adapter = require(options.cluster.adapter);
-        socket.adapter(adapter(app.datasources[options.cluster.datasource]));
-        console.info('Running in a clustered environment');
-      } catch (err) {
-        throw new Error(err);
-      }
-    }
+
     // Add a pubsub instanceable module
     app.pubsub = new Pubsub(socket, options);
+
     // Configure ioAuth
     if (options.auth) {
-      console.info('RTC authentication mechanism enabled');
+      debug("RTC authentication mechanism enabled");
       ioAuth(socket, {
         authenticate: (ctx, token, next) => {
           var AccessToken = app.models.AccessToken;
           //verify credentials sent by the client
-          var token = AccessToken.find({
-            where: { id: token.id ||  0, userId: token.userId ||  0 }
-          }, (err, tokenInstance) => next(err, tokenInstance.length > 0 ? true : false));
+          token = AccessToken.find({
+            where: { id: token.id || 0, userId: token.userId || 0 }
+          }, (err, tokenInstance) => {
+            next(err, tokenInstance.length > 0 ? true : false);
+          });
         },
         postAuthenticate: () => {
-          socket.on('authentication', value => {
-            console.info('A user (%s) has been authenticated over web sockets', value.userId);
+          socket.on("authentication", value => {
+            debug("A user (%s) has been authenticated over web sockets", value.userId);
           });
         }
       });
     }
 
-    socket.on('connection', connection => {
-      connection.on('lb-ping', () => connection.emit('lb-pong', new Date().getTime() / 1000));
+    socket.on("connection", connection => {
+      debug("A new client connected", connection);
+      connection.on("lb-ping", () => connection.emit("lb-pong", new Date().getTime() / 1000));
     });
   }
-}
+};
--- pubsub.js
+++ pubsub.js
@@ -1,21 +1,23 @@
-'use strict';
-module.exports = function Pubsub(socket, rootOptions) {
-    Pubsub.prototype.publish = function publish(options, next) {
-        if (options && options.method && options.endpoint && options.data) {
-            if (options.endpoint.match(/\?/))
-            options.endpoint = options.endpoint.split('?').shift();
-            let event = `[${options.method}]${options.endpoint}`;
-            // TODO Add DEBUG Config to enable the following from the component config
-            if (rootOptions.debug) {
-                console.info('Sending message to', event);
-                console.info('message', options.data);
-            }
-            socket.emit(event, options.data);
-            next();
-        } else {
-            console.info(options);
-            console.error('Error: Option must be an instance of type { method: string, data: object }');
-            next();
-        }
-    };
+"use strict";
+
+var debug = require("debug")("lc:pubsub");
+
+module.exports = function Pubsub(socket) {
+  Pubsub.prototype.publish = function publish(options, next) {
+    if (options && options.method && options.endpoint && options.data) {
+      if (options.endpoint.match(/\?/))
+        options.endpoint = options.endpoint.split("?").shift();
+      var event = `[${options.method}]${options.endpoint}`;
+
+      debug("Sending message to", event);
+      debug("message", options.data);
+
+      socket.emit(event, options.data);
+      next();
+    } else {
+      debug(options);
+      debug("Error: Option must be an instance of type { method: string, data: object }");
+      next();
+    }
+  };
 };
