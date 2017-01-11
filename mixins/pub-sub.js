c764fbfe938cc609d5d483c24ba2d3951622a29f
Robert McLeod <robert@autogrow.com>
2017-01-12 10:30:24 +1300

    add beforeRemote hook to retrieve instance data prior to deletion

--- mixins/pub-sub.js
+++ mixins/pub-sub.js
@@ -38,6 +38,40 @@ module.exports = function (Model, options) {
 
   options = Object.assign({ filters: {} }, options);
 
+  Model.beforeRemote("**", function(ctx, remoteMethodOutput, next) {
+
+    // look at delete requests to send the data for the deleted instances with
+    // with the request response.  Data is passed to the afterRemote hook via
+    // hookState in the context, as with remote methods.
+    if ( ctx && ctx.req.method == "DELETE" ) {
+      ctx.hookState = {};
+
+      // detect a relation being deleted
+      if (ctx.methodString.match(/__(destroy)__/g)) {
+
+        // we want to get the relations data, and we know the destroy method, so
+        // just replace destroy with get and we can get that data.
+        var getterMethod = ctx.method.name.replace("__destroy__", "__get__");
+        if ( typeof ctx.instance[getterMethod] !== "function" ) {
+          debug("cannot get doomed instance, %s does not have a method %s", ctx.instance.constructor.modelName, getterMethod);
+        }
+
+        // put the doomed relations data into the hookstate
+        ctx.instance[getterMethod](function(err, doomedRelation){
+          ctx.hookState.destroyed_data = doomedRelation;
+          return next && next();
+        });
+
+      } else {
+        ctx.hookState.destroyed_data = ctx.instance;
+        return next && next();
+      }
+
+    } else {
+      return next && next();
+    }
+  });
+
   Model.afterRemote("**", (ctx, remoteMethodOutput, next) => {
     if (shouldIgnoreRequest(ctx)) return next();
 
